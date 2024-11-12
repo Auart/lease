09d58adc2dc90b8626572cb8189755ec2c6abc76
Auart <taibai2048@gmail.com>
2024-11-12 21:37:01 +0800

    后台管理-初始配置-Springboot

---
+++ web/web-admin/src/main/java/cn/sdstudio/AdminWebApplication.java
@@ -0,0 +1,16 @@
+package cn.sdstudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Description: TODO
+ *
+ * @author auart
+ */
+@SpringBootApplication
+public class AdminWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminWebApplication.class, args);
+    }
+}
